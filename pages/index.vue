019ccf95308023639880a9f9f913e47612524021
Naoki Haba <naosprintrunner6385@gmail.com>
2023-03-20 01:22:40 +0900

    feat: コンポーネントにAxiosを埋め込み

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,20 @@
 <template>
-  <Tutorial/>
+  <section>
+    <div class="container"></div>
+  </section>
 </template>
 
 <script>
+import ROUTES from "~/routes/api";
+
 export default {
-  name: 'IndexPage'
+  async fetch({store}) {
+    const payload = {
+      uri: ROUTES.GET.POPULARS
+    }
+
+    // Vuex StoreのfetchPopularVideos Actionを実行
+    await store.dispatch('fetchPopularVideos', payload)
+  }
 }
 </script>
